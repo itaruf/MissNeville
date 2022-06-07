8ec21fbd89abf2e3e1bd95d77c2c273eebed3f60
itaruf <imane93100@hotmail.fr>
2022-06-07 16:26:54 +0200

    Update StateInventory.cpp

--- GameTest/StateInventory.cpp
+++ GameTest/StateInventory.cpp
@@ -19,55 +19,62 @@ void StateInventory::Update()
 
 void StateInventory::Render()
 {
-	auto bag = GameState::_player->_inventory->_bags[0];
+	auto player = GameState::_player;
+	auto bag = player->_inventory->_bags[0];
 	
 	float x = 700;
 	float y = 300;
-	float offset = 15;
+
+	float widthOff = 25;
+	float width = 4;
+	float height = 3;
+	float heightOff = 50;
 	size_t size = bag.second.size();
 
 	for (int i = 0; i < size; ++i)
 	{
 		if (i % 3 == 0)
 		{
-			y -= 25;
+			y -= heightOff;
 			x = 700;
-			offset = 15;
+			widthOff = 15;
 		}
 
 		// Upper line
 		App::DrawLine(
-			x + offset,
+			x + widthOff,
 			y,
-			x + 3 * offset,
+			x + width * widthOff,
 			y,
 			1, 1, 1);
 
 		// Left Line
 		App::DrawLine(
-			x + offset,
-			y - 35 + offset,
-			x + offset,
-			y - 35 + 2 * offset,
+			x + widthOff,
+			y - heightOff + widthOff,
+			x + widthOff,
+			y - heightOff + height * widthOff,
 			1, 1, 1);
 
 		// Right Line
 		App::DrawLine(
-			x + 3 * offset,
-			y - 35 + offset,
-			x + 3 * offset,
-			y - 35 + 2 * offset,
+			x + width * widthOff,
+			y - heightOff + widthOff,
+			x + width * widthOff,
+			y - heightOff + height * widthOff,
 			1, 1, 1);
 
 		// Down line
 		App::DrawLine(
-			x + offset,
-			y - 35 + offset,
-			x + 3 * offset,
-			y - 35 + offset,
+			x + widthOff,
+			y - heightOff + widthOff,
+			x + width * widthOff,
+			y - heightOff + widthOff,
 			1, 1, 1);
 
-		x = x + 2 * offset;
+		App::Print(((x + widthOff) + (x + width * widthOff)) / 2, ((y - heightOff + widthOff) + (y - heightOff + height * widthOff)) / 2, "Test");
+
+		x = x + width * widthOff;
 	}
 }
 
